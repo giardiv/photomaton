02a7964ad0a4d0967433b7b4b574f5a9c1ca4520
Vincent <giardina.v@gmail.com>
2019-04-13 19:32:58 +0200

    wip

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cv = require('opencv4nodejs');
+const npmRun = require('npm-run');
 
 const server = http.Server(app);
 const io = require('socket.io')(server);
@@ -13,19 +14,47 @@ const hostname = '127.0.0.1';
 
 const wCap = new cv.VideoCapture(0);
 
-app.use('/public',express.static('public'))
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+app.use('/public', express.static('public'))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+var opts = {
+    callbackReturn: "base64",
+    width: 1280,
+    height: 720,
+    quality: 10,
+    delay: 10,
+    saveShots: false,
+    output: "jpeg",
+    device: false,
+    verbose: true
+};
+
+var Webcam = NodeWebcam.create(opts);
+
+// NodeWebcam.capture("test_picture", opts, function (err, data) {
+//     var image = "<img src='" + data + "'>";
+// });
+
+npmRun.exec('Start-Process -FilePath "c:\docstoprint\doc1.pdf" -Verb Print', {cwd: __dirname }, function (err, stdout, stderr) {
+    // err Error or null if there was no error
+    // stdout Buffer|String
+    // stderr Buffer|String
+})
 
 setInterval(() => {
-    const frame = wCap.read();
-    console.log(frame);
-    const image = cv.imencode('.jpg', frame).toString('base64');
-    io.emit('image',image);
+    //const frame = wCap.read();
+    //console.log(frame);
+    //const image = cv.imencode('.jpg', frame).toString('base64');
 
-}, 100)
+   // Webcam.capture("test", opts, function (err, image) {
+    //    console.log(image);
+    //    io.emit('image', image);
+    //    Webcam.clear();
+   // });
+
+}, 1000)
 
 
 server.listen(port, hostname, () => {
